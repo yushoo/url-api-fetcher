9494ba2eb2f78d225457cdc2f1a171f6fb97934e
Henry Feng <henryfeng12345@gmail.com>
2020-01-01 16:25:27 -0600

    add bootstrap styling

--- url-api-fetcher/src/App.js
+++ url-api-fetcher/src/App.js
@@ -1,20 +1,31 @@
 import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import {Button, Container, Row, Col} from 'react-bootstrap';
 
 function App() {
   //state hook, count initialized to 0
-  const [count, setCount] = useState(0);
+  //const [count, setCount] = useState(0);
 
   //after button has pressed, after rendering, update the count
   useEffect(() => {
-    document.title = `${count}`;
+    //document.title = `${count}`;
   });
 
   return (
    <div> 
-    <p>Count: {count}</p>
-    <button onClick={() => setCount(count + 1)}>click here</button>
+     <Container>
+       <Row className="justify-content-md-center mt-5">
+         <Col></Col>
+         <Col md="auto">
+            <p>Fetcher</p>
+            <Button>fetch</Button>
+         </Col>
+         <Col></Col>
+        </Row>
+     </Container>
    </div>
   );
 
