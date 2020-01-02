3c199c95c5c4d45ffc7388038b7f2a815476ba74
Henry Feng <henryfeng12345@gmail.com>
2020-01-02 17:44:51 -0600

    add submit event handler

--- url-api-fetcher/src/App.js
+++ url-api-fetcher/src/App.js
@@ -6,30 +6,39 @@ import {Button, Row, Col, Form, Input} from 'antd';
 //import {Button, Container, Row, Col} from 'react-bootstrap';
 
 //test api url
-//'https://randomuser.me/api/'
+//  https://randomuser.me/api/
 
 function App() {
   //state hook, count initialized to 0
   //const [count, setCount] = useState(0);
   const [item, setItem] = useState(null);
+  const[temp, setTemp]  = useState('');
   const [url, setUrl]   = useState('');
 
   //after fetch button has pressed, await for the api to send information
-  useEffect(async (url) => {
-    const response = await fetch(url);
-    //turn response into json format
-    const data = await response.json();
-    console.log(data);
-    const item = data.results[0];
-    setItem(item);
-  }, []);
+  // useEffect(async (url) => {
+  //   const response = await fetch(url);
+  //   //turn response into json format
+  //   const data = await response.json();
+  //   console.log(data);
+  //   const item = data.results[0];
+  //   setItem(item);
+  // }, []);
 
   //makes to fetch api request
-  const newFetch = async (url) => {
+  const newFetch = async () => {
     const response = await fetch(url);
     const data = await response.json();
     const item = data.results[0];
     setItem(item);
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    //setUrl(temp);
+    newFetch();
+    //alert('WOKRINAGRASDGASDGGADSAGDS');
+    
   }
 
   return (
@@ -38,17 +47,20 @@ function App() {
          <Col span={4}></Col>
          <Col span={4}>
               <h1 className="mainContent" id="mainTitle">Fetcher</h1>
-              <Form.Item className="mainContent" id="fetchUrl" label="api url" >
-                <Input type="text" onChange={e => setUrl(e.target.value)}></Input>
-                {/* <p>{url}</p> */}
-                <div className="userInput">
-                  <Button className="" id="fetchButton" onClick={newFetch}>fetch</Button>
-                <p className="">Data:</p>
-                </div>
-                <div style={{ background: '#ffffff', padding: '0px', borderRadius: '10px' }}>
-                  {item && <div className="">{item.name.first}</div>}
-                </div>
-              </Form.Item>
+              <Form onSubmit={handleSubmit}>
+                <Form.Item className="mainContent" id="fetchUrl" label="api url"  >
+                  <Input type="text" onChange={e => setUrl(e.target.value)}></Input>
+                  {/* <p>{url}</p> */}
+                  <div className="userInput">
+                    <Button className="" id="fetchButton" htmlType="submit">fetch</Button>
+                    {/* <input type="submit" value="Submit" /> */}
+                  <p className="">Data:</p>
+                  </div>
+                  <div style={{ background: '#ffffff', padding: '0px', borderRadius: '10px' }}>
+                    {item && <div className="">{item.name.first}</div>}
+                  </div>
+                </Form.Item>
+              </Form>
          </Col>
          <Col span={4}></Col>
         </Row>
