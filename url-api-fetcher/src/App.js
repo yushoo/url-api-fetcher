8c3565e1346da446fcfdcb180e8d4d5fc9aa61bb
Henry Feng <henryfeng12345@gmail.com>
2020-01-02 16:06:14 -0600

    add styling, form, and input

--- url-api-fetcher/src/App.js
+++ url-api-fetcher/src/App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import 'antd/dist/antd.css';
-import {Button, Row, Col} from 'antd';
+import {Button, Row, Col, Form, Input} from 'antd';
 //import {Button, Container, Row, Col} from 'react-bootstrap';
 
 function App() {
@@ -29,16 +29,22 @@ function App() {
   }
 
   return (
-   <div className="outerContainer"> 
+   <div className="outerContainer" style={{ background: '#5cdbd3', padding: '30px' }}> 
        <Row Row type="flex" justify="space-between">
          <Col span={4}></Col>
          <Col span={4}>
-              <h1 className="mainContent">Fetcher</h1>
-              <div className="mainContent">
-                <Button className="mainContent" onClick={newFetch}>fetch</Button>
-              </div>
-              <p className="mainContent">Data</p>
-              {item && <div className="mainContent">{item.name.first}</div>}
+              <h1 className="mainContent" id="mainTitle">Fetcher</h1>
+              <Form.Item className="mainContent" id="fetchUrl" label="api url" >
+                <Input></Input>
+                <div className="userInput">
+                  <Button className="" id="fetchButton" onClick={newFetch}>fetch</Button>
+                <p className="">Data:</p>
+                </div>
+                <div style={{ background: '#ffffff', padding: '0px', borderRadius: '10px' }}>
+                  {item && <div className="">{item.name.first}</div>}
+                </div>
+              </Form.Item>
+              
          </Col>
          <Col span={4}></Col>
         </Row>
